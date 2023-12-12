df3075e336d43ec4c1a143c7bf5a9f05c7725afa
MegablasterMan1 <tryjhnsn12@gmail.com>
2023-12-12 12:38:30 -0600

    Added logic to lists

--- src/offerdrawer.tsx
+++ src/offerdrawer.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useState } from 'react';
 import { Global } from '@emotion/react';
 import { styled } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -8,78 +9,40 @@ import Box from '@mui/material/Box';
 import Skeleton from '@mui/material/Skeleton';
 import Typography from '@mui/material/Typography';
 import SwipeableDrawer from '@mui/material/SwipeableDrawer';
-import { Search, Style } from '@mui/icons-material';
+import { Style } from '@mui/icons-material';
 import { AppBar, Avatar, Grid, IconButton, Input, ListItem, ListItemText, Stack, TextField, Toolbar } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 import MenuIcon from '@mui/icons-material/Menu';
 
+import MonetizationOnIcon from '@mui/icons-material/MonetizationOn';
+import StyleIcon from '@mui/icons-material/Style';
+import DiamondIcon from '@mui/icons-material/Diamond';
+
 // Data Needed---------------
-let itemName = "Guppy Items"; // Name for the offer item
-let itemType = "T"; //Letter for Item in offer (Treasure = T, Loot = L, Coin = C, Soul = S)
-let itemRarity = { color: "black", bgcolor: 'lightyellow' };
-
-let coinCount = 12;
-let lootCount = 2;
-let soulCount = 1;
-// -------------------------
-
+const drawerBleeding = 56;
+const itemName = "Guppy Items"; // Name for the offer item
+const itemType = "T"; // Letter for Item in offer (Treasure = T, Loot = L, Coin = C, Soul = S)
+const itemRarity = { color: "black", bgcolor: 'lightyellow' };
+const lootCount = 2;
+const soulCount = 1;
 
 // OBJECT STRUCTURE -----------
-const ListObject = (iT: string, iR: object, iN: string) => {
-    console.log("IT: "+iT,"IR: "+iR,"IN: "+iN);
-    return(
-        <Stack
-            spacing={2} paddingY={"10px"} direction="row" alignItems="center" alignSelf={"start"}>
-            <Stack>
-                <Avatar sx={iR}>{iT}</Avatar>
-            </Stack>
-            <Stack sx={{ minWidth: 0 , flexGrow: 1}}>
-                <Typography variant='h6' noWrap>{iN}</Typography>
-            </Stack>
-            <Stack sx={{ minWidth: 0 }}>
-                <Button variant="contained" color="error" startIcon={<DeleteIcon />}>
-                    Delete
-                </Button>
-            </Stack>
-        </Stack>
-    );
-};
-// COUNT OBJ STRUCTURE -----------
-const ListCount = (iT: string, iN: string, cC: number, lC: number, sC: number) => {
-
-    let name = ("Coin: "+ cC);
-    let type = "C";
-
-
-    console.log("IT: "+iT,"IN: "+iN);
-    return(
-        <Stack
-            spacing={2} paddingY={"10px"} direction="row" alignItems="center" alignSelf={"start"}>
-            <Stack>
-                <Avatar sx={{ color: "black", bgcolor: 'lightgrey' }}>{type}</Avatar>
-            </Stack>
-            <Stack sx={{ minWidth: 0 , flexGrow: 1}}>
-                <Typography variant='h6' noWrap>{name}</Typography>
-            </Stack>
-            <Stack sx={{ minWidth: 0 }}>
-                <Button variant="contained" color="error" startIcon={<DeleteIcon />}>
-                    Delete
-                </Button>
-            </Stack>
-        </Stack>
-    );
-};
-
-// {ListObject(itemType, itemRarity, itemName)}
-
-
-
-
-
-
-
-
-const drawerBleeding = 56;
+const ListObject = (iT: string, iR: object, iN: string) => (
+    <Stack spacing={2} paddingY="10px" direction="row" alignItems="center" alignSelf="start">
+    <Stack>
+      <Avatar sx={iR}>{iT}</Avatar>
+    </Stack>
+    <Stack sx={{ minWidth: 0, flexGrow: 1 }}>
+      <Typography variant="h6" noWrap>{iN}</Typography>
+    </Stack>
+    <Stack sx={{ minWidth: 0 }}>
+      <Button variant="contained" color="error" startIcon={<DeleteIcon />}>
+        Delete
+      </Button>
+    </Stack>
+  </Stack>
+);
+// -----------
 
 const StyledBox = styled(Box)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'light' ? '#fff' : grey[800],
@@ -92,120 +55,206 @@ export default function SwipeableEdgeDrawer() {
       setOpen(newOpen);
     };
 
-  return (
+
+    //Get user information in offer--------------
+    const [coinAmount, setCoinAmount] = useState('');
+    const [lootAmount, setLootAmount] = useState('');
+    const [treasureName, setTreasureName] = useState('');
+    const [modifiedElements, setModifiedElements] = useState<React.ReactNode[]>([]);
+
+    // Delete Button
+    const handleDeleteButtonClick = (element: React.ReactNode) => {
+        const updatedElements = modifiedElements.filter((el) => el !== element);
+        setModifiedElements(updatedElements);
+    };
+
+    
+    const handleCoinButtonClick = () => {
+        const modifiedElement = (
+            <Stack
+            key={modifiedElements.length}
+            spacing={2}
+            paddingY="10px"
+            direction="row"
+            alignItems="center"
+            alignSelf="start"
+            >
+            <Stack>
+                <Avatar sx={{ color: 'black' }}>C</Avatar>
+            </Stack>
+            <Stack sx={{ minWidth: 0, flexGrow: 1 }}>
+                <Typography variant="h6" noWrap>
+                {'Coin: ' + Number(coinAmount)}
+                </Typography>
+            </Stack>
+            </Stack>
+        );
+
+        setModifiedElements([...modifiedElements, modifiedElement]);
+        setCoinAmount(''); // Clear input
+    };
+  
+    const handleLootButtonClick = () => {
+        const modifiedElement = (
+          <Stack
+            key={modifiedElements.length}
+            spacing={2}
+            paddingY="10px"
+            direction="row"
+            alignItems="center"
+            alignSelf="start"
+          >
+            <Stack>
+              <Avatar sx={{ color: "black", bgcolor: 'lightblue' }}>L</Avatar>
+            </Stack>
+            <Stack sx={{ minWidth: 0, flexGrow: 1 }}>
+              <Typography variant="h6" noWrap>{"Loot: " + Number(lootAmount)}</Typography>
+            </Stack>
+          </Stack>
+        );
+    
+        setModifiedElements([...modifiedElements, modifiedElement]);
+        setLootAmount(''); // Clear input
+    };
+  
+    const handleTreasureButtonClick = () => {
+      // Do something with treasureName
+      console.log('Treasure Name:', treasureName);
+    };
+
+
+return (
     <>
         <Global
-            styles={{
+        styles={{
             '.MuiDrawer-root > .MuiPaper-root': {
-                height: `calc(50% - ${drawerBleeding}px)`,
-                overflow: 'visible',
+            height: `calc(50% - ${drawerBleeding}px)`,
+            overflow: 'visible',
             },
-            }}
+        }}
         />
-
+    
         <Button
-            sx={{bgcolor: 'gold', color: 'black'}}
-            style={{ paddingInline: "5rem" }}
-            variant='contained'
-            size='large'
-            startIcon={<Style/>}
-            onClick={toggleDrawer(true)}
-            >Make Offer
-        </Button>
-
-        <SwipeableDrawer
-            anchor="bottom"
-            open={open}
-            onClose={toggleDrawer(false)}
-            onOpen={toggleDrawer(true)}
-            swipeAreaWidth={drawerBleeding}
-            disableSwipeToOpen={false}
-            ModalProps={{
-            keepMounted: true,
-            }}
+        sx={{ bgcolor: 'gold', color: 'black' }}
+        style={{ paddingInline: '5rem' }}
+        variant='contained'
+        size='large'
+        startIcon={<Style />}
+        onClick={toggleDrawer(true)}
         >
-            <StyledBox sx={{
-                pt: 4,
-                px: 2,
-                pb: 2,
-                height: '100%',
-                overflow: 'auto',
-                bgcolor: "white"
-            }}
-            >
+        Make Offer
+        </Button>
+    
+        <SwipeableDrawer
+        anchor="bottom"
+        open={open}
+        onClose={toggleDrawer(false)}
+        onOpen={toggleDrawer(true)}
+        swipeAreaWidth={drawerBleeding}
+        disableSwipeToOpen={false}
+        ModalProps={{
+            keepMounted: true,
+        }}
+        >
+        <StyledBox sx={{ pt: 4, px: 2, pb: 2, height: '100%', overflow: 'auto', bgcolor: 'white' }}>
             {/* _____________________ DRAWER CONTENT _____________________ */}
-
-            {/* sx={{ marginTop: 'auto' }} */}
-                <Grid container>
-                    <Grid container xs={12} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-
-                    {/* _____________________________________________ShopKeeper Offer________________________________ */}
-                        <Grid item xs={12} sm={6}>
-                            <Stack
-                                spacing={1}
-                                direction="column"
-                                justifyContent="center"
-                                alignItems="center"
-                                bgcolor={'grey'}
-                                borderRadius={2}
-                            >
-                                {/* ------------OBJ------------- */}
-                                {ListObject(itemType, itemRarity, itemName)}
-                                {ListObject(itemType, itemRarity, itemName)}
-                                {ListCount(itemType, itemName, coinCount, lootCount, soulCount)}
-                            </Stack>
-                        </Grid>
-
-                    {/* _____________________________________________Player Offer________________________________ */}
-                        <Grid item xs={12} sm={6}>
-                                <Stack
-                                    spacing={1}
-                                    direction="column"
-                                    justifyContent="center"
-                                    alignItems="center"
-                                    bgcolor={'grey'}
-                                    borderRadius={2}
-                                >
-                                    {/* ------------OBJ------------- */}
-                                    {ListCount(itemType, itemName, coinCount, lootCount, soulCount)}
-                                    {ListObject(itemType, itemRarity, itemName)}
-                                </Stack>
-                            </Grid>
-                    </Grid>
-
-
-                    <Grid container xs={12} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-
-                        <Grid item xs={12} sm={6}>
-                            <AppBar position="static" color='primary'>
-                                <Toolbar>
-                                    <Stack spacing={3} direction="row" borderRadius={2}>
-                                        <Button variant="contained" color="info">Item 1</Button>
-                                        <Button variant="contained" color="info">Item 1</Button>
-                                        <div>
-                                            <TextField
-                                            label="Item Name"
-                                            defaultValue=""
-                                            size="small"
-                                            />
-                                        </div>
-                                        <Button variant="contained" color="info">Item 1</Button>
-                                    </Stack>
-
-                                </Toolbar>
-                            </AppBar>
-                        </Grid>
-
-                        <Grid item xs={12} sm={6}>
-                            <Typography bgcolor={"blue"}>Ply UI</Typography>
-                        </Grid>
-                        
-                    </Grid>
+            <Grid container>
+            <Grid container xs={12} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+                {/* _____________________________________________ShopKeeper Offer________________________________ */}
+                <Grid item xs={12} sm={6}>
+                <Grid container bgcolor={'grey'}>
+                    {modifiedElements.map((element, index) => (
+                    <Stack key={index} bgcolor={'lightgrey'} borderRadius={1} margin={1.2}>
+                        {element}
+                        <Button
+                        variant="contained"
+                        color="error"
+                        startIcon={<DeleteIcon />}
+                        onClick={() => handleDeleteButtonClick(element)}
+                        sx={{ m: '10px' }}
+                        >
+                        Delete
+                        </Button>
+                    </Stack>
+                    ))}
                 </Grid>
-
-
-            </StyledBox>
+                </Grid>
+    
+                {/* _____________________________________________Player Offer________________________________ */}
+                <Grid item xs={12} sm={6}>
+                <Stack spacing={1} direction="column" justifyContent="center" alignItems="center" bgcolor={'grey'} borderRadius={2}>
+                    {/* ------------OBJ------------- */}
+                    {ListObject(itemType, itemRarity, itemName)}
+                </Stack>
+                </Grid>
+            </Grid>
+    
+            <Grid container xs={12} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+                <Grid item xs={12} sm={6}>
+                <AppBar position="static" color="primary">
+                    <Toolbar>
+                    <Stack spacing={0} direction="row" borderRadius={2}>
+                        {/* ----------COIN---------- */}
+                        <div>
+                        <TextField
+                            id="standard-number"
+                            label="Coins"
+                            value={coinAmount}
+                            type="number"
+                            onChange={(e) => setCoinAmount(e.target.value)}
+                            size="small"
+                            InputLabelProps={{
+                            shrink: true,
+                            }}
+                            variant="standard"
+                        />
+                        </div>
+                        <Button
+                        variant="contained"
+                        color="info"
+                        sx={{ marginRight: '10px' }}
+                        startIcon={<MonetizationOnIcon />}
+                        onClick={handleCoinButtonClick}
+                        ></Button>
+                        {/* ----------LOOT---------- */}
+                        <div>
+                        <TextField
+                            id="standard-number"
+                            label="Loot Cards"
+                            value={lootAmount}
+                            type="number"
+                            onChange={(e) => setLootAmount(e.target.value)}
+                            size="small"
+                            InputLabelProps={{
+                            shrink: true,
+                            }}
+                            variant="standard"
+                        />
+                        </div>
+                        <Button
+                        variant="contained"
+                        color="info"
+                        sx={{ marginRight: '10px' }}
+                        startIcon={<StyleIcon />}
+                        onClick={handleLootButtonClick}
+                        ></Button>
+                        {/* ----------TREASURE---------- */}
+                        <div>
+                        <TextField label="Treasure Name" value={treasureName} onChange={(e) => setTreasureName(e.target.value)} size="small" />
+                        </div>
+                        <Button variant="contained" color="info" startIcon={<DiamondIcon />} onClick={handleTreasureButtonClick}></Button>
+                    </Stack>
+                    </Toolbar>
+                </AppBar>
+                </Grid>
+    
+                <Grid item xs={12} sm={6}>
+                <Typography bgcolor={'blue'}>Ply UI</Typography>
+                </Grid>
+            </Grid>
+            </Grid>
+        </StyledBox>
         </SwipeableDrawer>
     </>
-  );
+    );
 }
